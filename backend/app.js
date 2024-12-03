140d5505e138516067081bda9877f6c541e0a639
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-12-03 15:13:38 +0200

    created backend server

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+import fs from "node:fs/promises";
+
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+
+app.use(express.static("images"));
+app.use(bodyParser.json());
+
+// CORS
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  next();
+});
+
+app.get("/places", async (req, res) => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const fileContent = await fs.readFile("./data/places.json");
+
+  const placesData = JSON.parse(fileContent);
+
+  res.status(200).json({ places: placesData });
+});
+
+app.listen(3000);
