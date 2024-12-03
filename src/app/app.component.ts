13dab9cab7e4068e7d543222f41423c81fae408c
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-12-03 16:37:35 +0200

    added user places component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AvailablePlacesComponent } from './places/available-places/available-places.component';
+import { UserPlacesComponent } from './places/user-places/user-places.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AvailablePlacesComponent],
+  imports: [AvailablePlacesComponent, UserPlacesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
